d47267dc6858f7f7915f473e61c35d2052eb81b8
Thiago Souza <thiagorasouza@gmail.com>
2024-12-21 00:53:59 +0000

    feat: add posts list layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { lexendSans } from "@/app/fonts";
 import { Github } from "@/components/icons/Github";
 import { LinkedIn } from "@/components/icons/LinkedIn";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="mx-auto flex min-h-screen max-w-[1200px] flex-col items-stretch gap-10 bg-gray-400 p-10">
-      <header className="flex items-center gap-3 bg-blue-400 py-8 text-4xl font-semibold">
+    <div className="mx-auto flex min-h-screen max-w-[1200px] flex-col items-stretch gap-10 p-10">
+      <header className="flex items-center gap-3 py-8 text-4xl font-semibold">
         <h1 className={lexendSans.className}>Thiago&apos;s Blog</h1>
         <nav className="ml-auto flex gap-9">
           <Link href="https://www.linkedin.com/in/thiagorasouza/">
@@ -17,8 +18,37 @@ export default function HomePage() {
           </Link>
         </nav>
       </header>
-      <section className="min-h-screen bg-red-400"></section>
-      <footer className="min-h-[200px] bg-green-400"></footer>
-    </main>
+      <main className="py-10">
+        <article className="flex flex-col gap-6">
+          <header className="flex items-center">
+            <h2 className={cn(lexendSans.className, "text-2xl font-semibold")}>
+              From Thinking React to Thinking Web Components
+            </h2>
+            <p className="ml-auto text-gray-400">Dec 20, 2024</p>
+          </header>
+          <div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              voluptate ex ad magni esse praesentium animi totam facere quas
+              debitis placeat ducimus dolore quisquam ratione, distinctio
+              dolorem, odio quae harum!
+            </p>
+          </div>
+          <div>
+            <ul
+              className={cn(
+                lexendSans.className,
+                "flex gap-5 text-sm *:rounded-2xl *:bg-gray-300 *:px-7 *:py-2 *:leading-none",
+              )}
+            >
+              <li>React</li>
+              <li>Javascript</li>
+              <li>WebComponents</li>
+            </ul>
+          </div>
+        </article>
+      </main>
+      <footer></footer>
+    </div>
   );
 }
